629153f866c16f434bb0b00dbd48f2e39036f1c8
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2023-09-08 19:11:34 +0530

    Add navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { buttonVariants } from "@ui/Button";
+import SignInButton from "@components/SignInButton";
+import SignOutButton from "@components/SignOutButton";
 
 const Navbar = async ({}) => {
   const session = await getServerSession();
@@ -7,23 +10,21 @@ const Navbar = async ({}) => {
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 h-20 right-0 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container mx-auto max-w-7xl w-full flex justify-between items-center">
-        <Link href="/" className={buttonVariants({ variants: "link" })}>
+        <Link href="/" className={buttonVariants({ variant: "link" })}>
           Text Similarity 1.0
         </Link>
-        <div className="md:hidden">
-          <ThemeToggle />
-        </div>
+        <div className="md:hidden">{/* <ThemeToggle /> */}</div>
         <div className="hidden md:flex gap-4">
           <Link
             href="/documentation"
-            className={buttonVariants({ variants: "ghost" })}
+            className={buttonVariants({ variant: "ghost" })}
           >
             Documentation
           </Link>
           {session ? (
             <>
               <Link
-                className={buttonVariants({ variants: "ghost" })}
+                className={buttonVariants({ variant: "ghost" })}
                 href={"/dashboard"}
               >
                 Dashboard
